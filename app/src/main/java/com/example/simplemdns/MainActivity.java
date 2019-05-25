60721c01be6717535e23f954d74256cefeb52da1
Bekket <jackatwork@yandex.ru>
2019-05-25 19:07:07 +0300

    added import

--- app/src/main/java/com/example/simplemdns/MainActivity.java
+++ app/src/main/java/com/example/simplemdns/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import p2mobile.P2mobile;
+
+
+//import   ;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }

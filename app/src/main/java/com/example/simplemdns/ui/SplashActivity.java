b4249ffc61ca9e63b71b6173f63daabf6722e5e9
ChronosX88 <chronosx88@gmail.com>
2019-07-03 12:59:01 +0400

    feat: Add on-start splash screen

---
+++ app/src/main/java/com/example/simplemdns/ui/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.simplemdns.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+                startActivity(new Intent(SplashActivity.this, P2ChatActivity.class));
+                SplashActivity.this.finish();
+            }
+        }.execute();
+    }
+}

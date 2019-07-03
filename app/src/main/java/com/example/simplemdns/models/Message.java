6d6c17875644dc7a2617bc357b0fd13485aabedd
ChronosX88 <chronosx88@gmail.com>
2019-07-03 11:01:46 +0400

    fix: Change logic to new API of p2mobile
    Former-commit-id: 965e3156c86e2c33171e83e8101ea3a3e982c508

--- app/src/main/java/com/example/simplemdns/models/Message.java
+++ app/src/main/java/com/example/simplemdns/models/Message.java
@@ -1,9 +1,15 @@
 package com.example.simplemdns.models;
 
-public class Message {
-    public final String text;
+import com.google.gson.annotations.SerializedName;
 
-    public Message(String text) {
-        this.text = text;
+public class Message {
+    @SerializedName("Body") public String body;
+    @SerializedName("From") public String from;
+
+    public Message() { }
+
+    public Message(String body, String from) {
+        this.body = body;
+        this.from = from;
     }
 }
